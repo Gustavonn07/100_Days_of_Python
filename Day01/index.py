f3e15d0d58245b20eb6d0b4a8237eafed4126b19
Gustavonn07 <gustavonepnog@gmail.com>
2024-05-01 14:19:52 -0300

    add: starting first class

---
+++ Day01/index.py
@@ -0,0 +1,7 @@
+city:str
+pet:str
+
+print('Wellcome to the Band Name Generator!')
+city = input("What's the name of the city you grow up in? ")
+pet = input("What's your pet's name? ")
+print(f"Your band name should be {pet} {city}")
